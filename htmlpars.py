23a39a06fd81acb9b9c9f1bd6e987e848698b806
Pok <robert.wiessner1@gmx.de>
2016-03-26 04:38:00 +0100

    htmlparser gives episodes from a staffurl as dict

--- htmlpars.py
+++ htmlpars.py
@@ -1,10 +1,11 @@
 import html.parser
 import logging
 from html.entities import name2codepoint
+import re
 
 class HtmlParserHelper(html.parser.HTMLParser):
 
-    def __init__(self, loglevel=logging.DEBUG):
+    def __init__(self, loglevel=logging.INFO):
         html.parser.HTMLParser.__init__(self)
         self.__logger = logging.getLogger(__name__)
         self.__logger.setLevel(loglevel)
@@ -15,24 +16,70 @@ class HtmlParserHelper(html.parser.HTMLParser):
         shandler.setFormatter(formatter)
         if len(self.__logger.handlers) <= 0:
             self.__logger.addHandler(shandler)
+            # deniy multiple prints if the class initiated multiple
+
+        self.__currenttag_td = False
+        self.__epfound = False
+        self.__beginngathering = False
+        self.__episodes = {}
 
     def getepisodes(self, htmltext):
 
         pass
 
     def handle_starttag(self, tag, attrs):
+        if tag == "td":
+            self.__currenttag_td = True
+            for attr in attrs:
+                if attr[0] == "class":
+                    self.__logger.debug("attr[0] == class  passed")
+                    if attr[1] == "nowrap":
+                        self.__logger.debug("begingathering is true")
+                        self.__beginngathering = True
+
+        if tag == "a" and self.__epfound is True:
+            title = None
+            self.__logger.debug("a and epfound passing ")
+            for attr in attrs:
+                if  attr[0] == "title" and self.__beginngathering is True:
+                    self.__episodes[self.__currentepisode][attr[1]] = None
+                    title = attr[1]
+                if attr[0] == "href" and self.__beginngathering is True:
+                    if title == "Streamcloud":
+                        self.__logger.info("Streamcloudlink found: " + "http://bs.to/" + attr[1])
+                    self.__episodes[self.__currentepisode][title] = "http://bs.to/" + attr[1]
+                    title = None
         self.__logger.debug("Start tag:"+ tag)
         for attr in attrs:
             self.__logger.debug("     attr:"+ str(attr))
+
     def handle_endtag(self, tag):
+        if tag == "td":
+            self.__currenttag_td = False
+        if tag == "tr":
+            self.__epfound = False
+            self.__currentepisode = None
+            self.__beginngathering = False
         self.__logger.debug("End tag  :"+ tag)
+
     def handle_data(self, data):
+        if self.__currenttag_td and self.__epfound is False:
+            isnumber = re.match(r"\d+",data)
+            if isnumber:
+                self.__epfound = True
+                self.__currentepisode = isnumber.group()
+                self.__logger.debug("found a episode: " + self.__currentepisode)
+                self.__episodes[self.__currentepisode] = {}
         self.__logger.debug("Data     :"+ data)
+
+
     def handle_comment(self, data):
         self.__logger.debug("Comment  :"+ data)
+
     def handle_entityref(self, name):
         c = chr(name2codepoint[name])
         self.__logger.debug("Named ent:"+ c)
+
     def handle_charref(self, name):
         if name.startswith('x'):
             c = chr(int(name[1:], 16))
@@ -41,3 +88,7 @@ class HtmlParserHelper(html.parser.HTMLParser):
         self.__logger.debug("Num ent  :"+ c)
     def handle_decl(self, data):
         self.__logger.debug("Decl     :"+ data)
+
+    def get_episodes(self):
+        self.__logger.debug(self.__episodes)
+        return self.__episodes
